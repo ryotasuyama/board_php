79ad2f2d30ddebd84415042e6f440e36aa730a8a
ryota <f.s.ryota@gmail.com>
2024-05-11 22:39:40 +0900

    firstcommit

--- index.php
+++ index.php
@@ -15,24 +15,11 @@ $item = explode("\n<END>\n", $f);
 for ($i = 0; $i < count($item) - 1; $i++) {
     list($header, $body) = explode("\n", $item[$i], 2);
     list($date, $ip, $host, $name) = explode(',', $header);
+    list($text, $imgfile) = explode("\n$imgfile\n", $body);
     echo '<font color="#008000"><b>' . ($i + 1) . ": $date $host($ip) $name</b></font>\n";
-    echo "<br>";
-    echo nl2br(htmlspecialchars($body));
-    echo "<br>";
-    preg_match_all('/\bhttp:\/\/\S+\.pdf\b/', $body, $matches);
-    foreach ($matches[0] as $match) {
-        echo "<a href='$match'>PDFファイル</a><br>";
-    }
-    echo "<br>";
+    echo "$body\n";
+    echo "<img src ='upload/$imgfile'>\n";
 }
-
-$upload_dir = 'upload/';
-$img_pattern = '/\bhttp:\/\/\S+\.(jpg|jpeg|png|gif)\b/i';
-preg_match_all($img_pattern, $body, $img_matches);
-foreach ($img_matches[0] as $img_match) {
-    echo "<img src='$img_match' style='max-width: 100%;'><br>";
-}
-
 ?>
 </pre>
     <form enctype="multipart/form-data" action="5_submit.php" method="POST">
